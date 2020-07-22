12e144dd25b9a467e8678878583f52950382952b
Quockm <mjnhquoc21101997@gmail.com>
2020-07-22 18:46:29 +0700

    inthongbao

--- app/src/main/java/com/example/a22_07_2020/MainActivity.java
+++ app/src/main/java/com/example/a22_07_2020/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.lang.annotation.Annotation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int value=tinhtong(5,20);
+        int value=tinhtong(5,210);
         Log.d("BBB",String.valueOf(value));
-
+        inthongbao("ThongBao");
 
     }
     private int tinhtong(int a, int b)
     {
         int ketqua=a+b;
         return ketqua;
+    }
+    // Annotation dinh nghia chuc nang, cach su ly,...
+    private void inthongbao(String mes)
+    {
+        Toast.makeText(MainActivity.this,mes,Toast.LENGTH_LONG).show();
     }
 }
